2ae34ea51151646795467e96c02a796dfa0cc7c2
jhessiny <jhessinymattos@gmail.com>
2023-01-22 15:14:46 -0300

    feat: change background

--- src/app/sections/projects/projects.tsx
+++ src/app/sections/projects/projects.tsx
@@ -7,19 +7,24 @@ import { projectsList } from "./constants";
 export const ProjectsSection = () => {
   return (
     <div>
-      <Container>
-        <h2 className="font-bold text-secondary-dark text-3xl uppercase mb-4">
-          Portfolio
-        </h2>
-      </Container>
       <Divider />
-      <div className="bg-img bg-[url('/assets/trees-of-mystery.jpg')] bg-no-repeat bg-center bg-cover h-96  relative">
-        <div className="bg-gradient-to-r from-backdrop to-backdrop w-full h-full py-20  ">
+      <div className="bg-img bg-[url('/assets/almond-tree-branch.jpg')] bg-no-repeat bg-right-bottom bg-cover relative overflow-hidden">
+        <div className="bg-gradient-to-r from-backdrop to-backdrop w-full h-full pt-28 pb-20 ">
           <Container>
-            <div className="flex justify-between items-stretch gap-2">
-              {projectsList.map((item) => (
-                <ProjectItem key={item.title} {...item} />
-              ))}
+            <div className="flex">
+              <div className="flex items-end">
+                <h2
+                  className="font-bold text-text-dark text-5xl uppercase transform rotate-180 inline-block"
+                  style={{ writingMode: "vertical-rl" }}
+                >
+                  Portfolio
+                </h2>
+              </div>
+              <div className="flex justify-between items-stretch gap-2 ">
+                {projectsList.map((item) => (
+                  <ProjectItem key={item.title} {...item} />
+                ))}
+              </div>
             </div>
           </Container>
         </div>
