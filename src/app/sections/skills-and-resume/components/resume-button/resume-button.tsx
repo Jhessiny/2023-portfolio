8e6a531522cfe8aa2a437047b9d9abe8ed755508
jhessiny <jhessinymattos@gmail.com>
2023-01-22 15:15:28 -0300

    feat: create download resume button

---
+++ src/app/sections/skills-and-resume/components/resume-button/resume-button.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./resume.scss";
+
+export const ResumeButton = () => {
+  const [isActive, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(true);
+    setTimeout(function () {
+      setActive(false);
+    }, 2000);
+  };
+
+  const particles = new Array(35).fill(
+    <span key={Math.random()} className="particle"></span>
+  );
+
+  return (
+    <div className={`${isActive ? "active" : ""} relative`}>
+      <a
+        onClick={handleClick}
+        href="/assets/RESUME - JHÉSSINY MATTOS.pdf"
+        download="RESUME - JHÉSSINY MATTOS"
+        className="bg-primary-dark text-white px-10 py-5 uppercase rounded-sm hover:brightness-110 duration-300"
+      >
+        Download Resume
+      </a>
+      {particles}
+    </div>
+  );
+};
