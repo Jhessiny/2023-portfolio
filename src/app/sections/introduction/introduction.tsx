ea4d473d44bb3e1023c4709939196379310f58ff
jhessiny <jhessinymattos@gmail.com>
2023-01-30 23:25:20 -0300

    fix: move header outside the div with overflow hidden

--- src/app/sections/introduction/introduction.tsx
+++ src/app/sections/introduction/introduction.tsx
@@ -4,31 +4,34 @@ import Image from "next/image";
 
 export const IntroductionSection = () => {
   return (
-    <div className="h-[90vh] md:h-[900px] bg-tertiary bg-img bg-[url('/assets/avatar.png')] bg-no-repeat bg-center bg-cover md:bg-contain overflow-x-hidden relative">
+    <>
       <Header />
-
-      <div className="h-full w-full md:w-[50.5%] bg-primary-light opacity-95 md:opacity-80 md:animate-bounce-finite md:translate-x-[100%] absolute top-0">
-        <div className="h-full flex flex-col items-center justify-center ">
-          <div>
-            <p className="text-text-dark font-bold uppercase text-xs md:ml-3 animate-re-appear">
-              need an awesome
-            </p>
-            <p className="text-text-dark font-thin uppercase text-4xl leading-[2rem] md:text-6xl md:leading-[3.5rem] xl:text-8xl xl:leading-[5rem]">
-              <span className=" block animate-appearSlideRight">frontend</span>
-              <span className="block animate-appearSlideToLeft">
-                developer?
-              </span>
-            </p>
-            <Image
-              width={250}
-              height={250}
-              alt="signature"
-              src="/assets/signature.png"
-              className="h-48 object-cover animate-re-appear"
-            />
+      <div className="h-[90vh] md:h-[900px] bg-tertiary bg-img bg-[url('/assets/avatar.png')] bg-no-repeat bg-center bg-cover md:bg-contain relative overflow-hidden">
+        <div className="h-full w-full md:w-[50.5%] bg-primary-light opacity-95 md:opacity-80 md:animate-bounce-finite md:translate-x-[100%] absolute top-0">
+          <div className="h-full flex flex-col items-center justify-center ">
+            <div>
+              <p className="text-text-dark font-bold uppercase text-xs md:ml-3 animate-re-appear">
+                need an awesome
+              </p>
+              <p className="text-text-dark font-thin uppercase text-6xl leading-[3.5rem] xl:text-8xl xl:leading-[5rem]">
+                <span className=" block animate-appearSlideRight">
+                  frontend
+                </span>
+                <span className="block animate-appearSlideToLeft">
+                  developer?
+                </span>
+              </p>
+              <Image
+                width={250}
+                height={250}
+                alt="signature"
+                src="/assets/signature.png"
+                className="h-48 object-cover animate-re-appear"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
