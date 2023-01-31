ab61f946ed04dcafdcd323ae2e413d352ed7b341
jhessiny <jhessinymattos@gmail.com>
2023-01-30 23:24:36 -0300

    fix: adjust hamburger color and add close button

--- src/app/components/header/components/mobile-menu.tsx
+++ src/app/components/header/components/mobile-menu.tsx
@@ -1,23 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavItem } from "../../nav-item/nav-item";
+import { IoMdClose } from "react-icons/io";
+import { twMerge } from "tailwind-merge";
 
-export const MobileMenu = () => {
+export const MobileMenu = ({ isScrolled }: { isScrolled: boolean }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const hamburgerColor = isScrolled ? "bg-primary-light" : "bg-secondary-dark";
+
+  useEffect(() => {
+    if (isOpen) document.querySelector("body")!.style.overflow = "hidden";
+    else document.querySelector("body")!.style.overflow = "auto";
+  }, [isOpen]);
+
   return (
     <div className="block md:hidden">
       <div className="space-y-2" onClick={() => setIsOpen(true)}>
-        <div className="w-8 h-0.5 bg-primary-light"></div>
-        <div className="w-8 h-0.5 bg-primary-light"></div>
-        <div className="w-8 h-0.5 bg-primary-light"></div>
+        <div className={twMerge("w-8 h-0.5", hamburgerColor)}></div>
+        <div className={twMerge("w-8 h-0.5", hamburgerColor)}></div>
+        <div className={twMerge("w-8 h-0.5", hamburgerColor)}></div>
       </div>
       {isOpen && (
-        <div className="bg-primary-light sticky top-0 left-0">
-          <ul className="items-center flex flex-col justify-center ">
-            <NavItem href="/" text="Home" className="text-lg" />
-            <NavItem href="/" text="Experience" className="text-lg" />
-            <NavItem href="/" text="Portfolio" className="text-lg" />
-            <NavItem href="/" text="Resume" className="text-lg" />
-            <NavItem href="/" text="Blog" highlight className="text-lg" />
+        <div className="bg-primary-light fixed top-0 left-0 w-[100vw] h-[100vh] z-50 overflow-hidden">
+          <IoMdClose
+            size={40}
+            className="absolute top-2 right-2 cursor-pointer z-10"
+            onClick={() => setIsOpen(false)}
+            color="black"
+          />
+          <ul className="items-center flex flex-col justify-center h-full gap-10">
+            <NavItem href="/" text="Home" className="text-3xl" />
+            <NavItem href="/" text="Experience" className="text-3xl" />
+            <NavItem href="/" text="Portfolio" className="text-3xl" />
+            <NavItem href="/" text="Resume" className="text-3xl" />
+            <NavItem href="/" text="Blog" highlight className="text-3xl" />
           </ul>
         </div>
       )}
