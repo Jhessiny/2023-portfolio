af869974cad91cc0925ffe8ca1063c7849ec35d2
jhessiny <jhessinymattos@gmail.com>
2023-01-30 22:25:14 -0300

    feat: add className prop to NavItem

--- src/app/components/nav-item/nav-item.tsx
+++ src/app/components/nav-item/nav-item.tsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   href: string;
   text: string;
   highlight?: boolean;
   light?: boolean;
+  className?: string;
 };
 
-export const NavItem = ({ href, text, highlight, light }: Props) => {
-  const textColor = light ? `"text-primary-light` : "text-text-dark";
+export const NavItem = ({ href, text, highlight, light, className }: Props) => {
+  const textColor = light ? "text-primary-light" : "text-text-dark";
   const border = highlight ? "border-solid border-2 border-secondary-dark" : "";
   return (
     <li>
       <a
-        className={`${textColor} ${border} font-bold text-xs py-2 px-2 lg:px-4 uppercase inline-block leading-3`}
+        className={twMerge(
+          textColor,
+          border,
+          "font-bold py-2 px-2 lg:px-4 uppercase inline-block leading-3 text-xs",
+          className
+        )}
         href={href}
       >
         {text}
