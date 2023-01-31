26a0abfb3865adaf8b561186d3de25c486ae5e98
jhessiny <jhessinymattos@gmail.com>
2023-01-30 22:23:52 -0300

    feat: listen to scroll to add fixed to header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,22 +1,48 @@
-import React from "react";
-import { NavItem } from "../nav-item/nav-item";
-import { RxDotFilled } from "react-icons/rx";
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { twMerge } from "tailwind-merge";
+import { DefaultMenu, MobileMenu } from "./components";
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const checkScroll = () => {
+    if (window?.scrollY > 5) setIsScrolled(true);
+    else setIsScrolled(false);
+  };
+  useEffect(() => {
+    window?.addEventListener("scroll", checkScroll);
+    return window?.removeEventListener("scroll", checkScroll);
+  }, []);
+
+  const bgScrolledClasses = isScrolled
+    ? "fixed bg-secondary-dark bg-opacity-80 backdrop-blur-lg"
+    : "";
+  const titleScrolledClasses = isScrolled
+    ? "text-primary-light"
+    : "text-secondary-dark md:text-primary-light";
   return (
-    <nav className="flex justify-between px-2 py-4 absolute w-full max-w-ultraLargeDesktop z-10 translate-x-[-50%] left-[50%]">
-      <h1 className="text-secondary-dark md:text-primary-light font-bold text-3xl uppercase tracking-widest">
-        JOSM
-      </h1>
-      <ul className="hidden items-center animate-re-appear md:flex">
-        <RxDotFilled color="#42404b" className="mr-5 hidden lg:block" />
-        <NavItem href="/" text="Home" />
-        <NavItem href="/" text="Experience" />
-        <NavItem href="/" text="Portfolio" />
-        <NavItem href="/" text="Resume" />
-        <NavItem href="/" text="Blog" highlight />
-        <RxDotFilled color="#42404b" className="ml-5 hidden lg:block" />
-      </ul>
-    </nav>
+    <div
+      className={twMerge(
+        `absolute w-full z-10 translate-x-[-50%] left-[50%] duration-300 `,
+        bgScrolledClasses
+      )}
+    >
+      <nav
+        className={twMerge(
+          `flex justify-between px-2 py-4 w-full max-w-ultraLargeDesktop mx-auto`,
+          titleScrolledClasses
+        )}
+      >
+        <h1
+          className={twMerge(" font-bold text-3xl uppercase tracking-widest")}
+        >
+          JOSM
+        </h1>
+
+        <DefaultMenu isScrolled={isScrolled} />
+        <MobileMenu />
+      </nav>
+    </div>
   );
 };
